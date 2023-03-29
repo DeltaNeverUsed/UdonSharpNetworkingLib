2154d9384ab93cb23dc211662c93524f39bcbd0b
DeltaNeverUsed <sinus06@hotmail.com>
2023-03-29 17:18:48 +0200

    Patching The UdonSharp Compiler Working

---
+++ Editor/PreProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UdonSharp.Compiler;
+using UnityEditor;
+using UnityEngine;
+
+namespace USPPNet
+{
+    public static class PreProcessor
+    {
+        private static string USPPNetVars = @"
+";
+        
+        public static string Parse(string prog)
+        {
+            prog = prog.Replace("It worked 2!", "SomeWrong");
+            prog = prog.Replace("This is a test", "It worked 2!");
+            return prog;
+        }
+    }
+}
